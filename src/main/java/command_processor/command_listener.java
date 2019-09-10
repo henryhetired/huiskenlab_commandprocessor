9e4b2dd32523078d9526087286ee226f34c2f08d
henryhe5692wow@gmail.com <henryhe5692wow@gmail.com>
2019-09-10 16:10:19 -0500

    java implemented

--- src/main/java/command_processor/command_listener.java
+++ src/main/java/command_processor/command_listener.java
@@ -4,50 +4,180 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.*;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
 import ij.ImageJ;
 import ij.plugin.Macro_Runner;
+import javafx.util.Pair;
+
 public class command_listener implements Runnable{
+    protected config config = null;
     protected boolean isStopped= false;
     protected ServerSocket server = null;
+    protected Thread runningThread = null;
     protected int ServerPort = 53705;
-    private static Queue<String> commands;
-    private static ExecutorService myexecutor = Executors.newFixedThreadPool(5);
-    private static ExecutorService commandparser = Executors.newFixedThreadPool(2);
+    protected int threadport_start = 50400;
+    protected ReentrantLock printlock = new ReentrantLock();
+    protected BlockingQueue<commands> commands= null;
+    private class commands{
+        private String commands;
+        private int port;
+        public commands(String commandsin,int portin){
+            this.commands = commandsin;
+            this.port = portin;
+        }
+    }
+    protected BlockingQueue<Integer> ports = null;
+    private static ExecutorService commandexecutor = Executors.newFixedThreadPool(5);
+    private static ExecutorService scriptexecutor = Executors.newFixedThreadPool(5);
     //TCP/IP server class that listen to command for processing
-    public void init(config configin) {
+    public command_listener(config configin){
+        this.config = configin;
+        this.ServerPort = configin.port;
+    }
+    public void openServerSocket() {
         try {
-            InetAddress add = InetAddress.getByName(configin.ipadd)
-            server = new ServerSocket(configin.port,10,add);
-            String workspace = configin.workspace;
+            InetAddress add = InetAddress.getByName(this.config.ipadd);
+            this.server = new ServerSocket(config.port,10,add);
+            for (int i=0;i<5;i++){
+                try {
+                    this.ports.put(this.threadport_start+i);
+                }
+                catch (InterruptedException in) {
+                    in.printStackTrace();
+                }
+            }
             System.out.println("Server Started");
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(("Cannot open port" + config.port));
         }
-        myexecutor = Executors.newFixedThreadPool(5);
-        commandparser = Executors.newFixedThreadPool(2);
-        initialized = true;
-        commands = new LinkedList<String>();
+    }
+    public synchronized void stop(){
+        this.isStopped = true;
+        try {
+            this.server.close();
+        }catch (IOException e){
+            throw new RuntimeException("Error closing server",e);
+        }
+    }
+    private synchronized boolean isStopped(){
+        return this.isStopped;
     }
 
-    public void process_command(final String command) {
-        myexecutor.execute(new Runnable(){
+    private void command_accept(Socket acceptedClient){
+        //Accept commands from a socket and put it in the command processing queue
+        try{
+            InputStream is = acceptedClient.getInputStream();
+            BufferedReader d = new BufferedReader(new InputStreamReader(is));
+            String messagein = d.readLine();
+            if (messagein.startsWith("runscript")){
+                //Script command running, no need to maintain connection
+                OutputStream os = acceptedClient.getOutputStream();
+                os.write("Script processing command received.".getBytes());
+                os.close();
+                is.close();
+                process_script_command(messagein);
+            }
+            else if (messagein.startsWith("runcommand")){
+                try{
+                    //TODO:Implement cases where there is no workers available, ask it to wait
+                    int port = ports.take();
+                    commands.put(new commands(messagein,port));
+                    OutputStream os = acceptedClient.getOutputStream();
+                    String response = String.format("Connect to %d\n",port);
+                    os.write(response.getBytes());
+                    os.close();
+                    is.close();
+                }
+                catch (InterruptedException i){
+                    i.printStackTrace();
+                }
+            }
+            else if (messagein.startsWith("Terminate server")){
+                this.stop();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+    class command_worker implements Runnable{
+        private String command;
+        private String[] commandlist;
+        private int port;
+        private ServerSocket socket= null;
+        public command_worker(int port,String commandin){
+            this.command = commandin;
+            this.commandlist = command.split(" ");
+            this.port = port;
+            try {
+                InetAddress add = InetAddress.getByName(config.ipadd);
+                this.socket = new ServerSocket(this.port, 10, add);
+            }
+            catch(IOException e) {
+                throw new RuntimeException(("Cannot open port" + this.port));
+            }
+        }
+        public void run() {
+            if (this.commandlist[1].startsWith("file_writing_request")){
+                try {
+                    writefile(Integer.parseInt(commandlist[2]),Integer.parseInt(commandlist[3]),Integer.parseInt(commandlist[4]),commandlist[5],this.socket);
+                    this.socket.close();
+                }
+                catch (IOException e){
+                    throw new RuntimeException("Cannot close port" + this.port);
+                }
+                printlock.lock();
+                System.out.println("Command finished:"+command);
+                printlock.unlock();
+            }
+            //TODO:Implement other potential functions
+
+        }
+    }
+    private void writefile(int zsize,int ysize,int xsize,String filename,ServerSocket socket) throws IOException{
+        Socket clientsocket = null;
+        clientsocket = socket.accept();
+        FileOutputStream fos = new FileOutputStream(filename);
+        DataOutputStream out = new DataOutputStream(new BufferedOutputStream(clientsocket.getOutputStream()));
+        DataInputStream in = new DataInputStream(new BufferedInputStream(clientsocket.getInputStream()));
+        byte[] bytes = new byte[4096];
+        int chunksize = 2*xsize*ysize;
+        for (int i=0;i<zsize;i++){
+            int pos = 0;
+            while (pos<chunksize-1){
+                in.read(bytes);
+            }
+            fos.write(bytes);
+        }
+        fos.close();
+        out.write("Data received".getBytes());
+        out.close();
+        in.close();
+        return;
+
+    }
+    public void process_script_command(final String command) {
+        scriptexecutor.execute(new Runnable(){
             @Override
             public void run(){
                 String[] current_command = command.split("\\ ");
                 String args ="";
-                for (int i=1;i<current_command.length;i++){
+                for (int i=2;i<current_command.length;i++){
                     args+=current_command[i];
                     args+=" ";
                 }
+                args = args.trim();
                 if (current_command[0].endsWith(".ijm")){
-                    String pluginsDir = workspace;
+                    String pluginsDir = config.workspace;
                     System.setProperty("plugins.dir", pluginsDir);
                     ImageJ session = new ImageJ(ImageJ.NO_SHOW);
                     Macro_Runner mr = new Macro_Runner();
-                    mr.runMacroFile(workspace + current_command[0]+".ijm",args);
+                    mr.runMacroFile(config.workspace + current_command[0]+".ijm",args);
                     session.quit();
                 }
                 else if (current_command[0].endsWith(".py")){
@@ -65,54 +195,49 @@ public class command_listener implements Runnable{
         });
     }
 
-    public void command_handler() {
-        commandparser.execute(new Runnable() {
-            @Override
+    class command_handler implements Runnable{
             public void run() {
-                String current_command;
-                while (keeprunning) {
+                while (!isStopped()) {
                     try {
-                        current_command = commands.remove();
-                        process_command(current_command);
-                        System.out.println(current_command);
-                        System.out.println("Command started");
-                    } catch (NoSuchElementException e) {
-                        continue;
-                    }
-                    try {
-                        Thread.sleep(100);
-                    } catch (Exception e) {
+                        commands current_command = commands.take();
+                        commandexecutor.execute(new command_worker(current_command.port, current_command.commands));
+                        printlock.lock();
+                        System.out.println("Command started" + current_command.commands);
+                        printlock.unlock();
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
 
                 }
-                return;
             }
-        });
-    }
-
-    public void run() {
-        command_handler();
-        if (initialized) {
-            while (!messagein.contains("disconnect")) {
-                try {
-                    client = server.accept();
-                    System.out.println("client connected" + client.getInetAddress());
-                    is = new DataInputStream(client.getInputStream());
-                    BufferedReader d = new BufferedReader(new InputStreamReader(is));
-                    messagein = d.readLine();
-                    commands.add(messagein);
-                    System.out.println("commands queued");
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            keeprunning = false;
-            commandparser.shutdown();
-            myexecutor.shutdown();
-            System.out.println("Server stopped");
-            return;
         }
+    public void run(){
+        synchronized(this){
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+        command_handler ch = new command_handler();
+        Thread handlerthread = new Thread(ch);
+        handlerthread.start();
+        while(! isStopped()){
+            Socket clientSocket = null;
+            try {
+                clientSocket = this.server.accept();
+            } catch (IOException e) {
+                if(isStopped()) {
+                    System.out.println("Server Stopped.") ;
+                    break;
+                }
+                throw new RuntimeException(
+                        "Error accepting client connection", e);
+            }
+            command_accept(clientSocket);
+        }
+        this.commandexecutor.shutdown();
+        printlock.lock();
+        System.out.println("Server Stopped.") ;
+        printlock.unlock();
     }
-}
+    }
+
+
